f94a7d680b8ad0ecebeef868ce2752e828b7868a
izumin5210 <m@izum.in>
2018-12-02 11:28:34 +0900

    Add `NewVersionCommand()`

---
+++ pkg/cli/version.go
@@ -0,0 +1,42 @@
+package cli
+
+import (
+	"bytes"
+	"fmt"
+	"runtime"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+// Build is a container for the application build information.
+type Build struct {
+	AppName   string
+	Version   string
+	Revision  string
+	BuildDate string
+}
+
+// NewVersionCommand create a new cobra.Command to print the version information.
+func NewVersionCommand(io IO, cfg Build) *cobra.Command {
+	return &cobra.Command{
+		Use:           "version",
+		Short:         "Print the version information",
+		Long:          "Print the version information",
+		SilenceErrors: true,
+		SilenceUsage:  true,
+		Run: func(cmd *cobra.Command, _ []string) {
+			buf := bytes.NewBufferString(cfg.AppName + " " + cfg.Version)
+			buf.WriteString(" (")
+			var meta []string
+			for _, c := range []string{cfg.BuildDate, cfg.Revision, runtime.Version(), fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH)} {
+				if c != "" {
+					meta = append(meta, c)
+				}
+			}
+			buf.WriteString(strings.Join(meta, " "))
+			buf.WriteString(")")
+			fmt.Fprintln(io.Out(), buf.String())
+		},
+	}
+}
